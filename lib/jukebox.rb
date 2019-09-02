6566f083cf69e76dc55a83255dc272a6d15a8b89
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-02 01:50:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ def play(songs)
     puts "Playing #{resp}"
     
   elsif songs[resp.to_i - 1]
-    puts "Resp: #{resp.to_i-1"
+    puts "Resp: #{resp.to_i-1}"
     puts "Playing #{songs[resp.to_i - 1]}"
     
   else
