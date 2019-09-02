c38ceb00442b8c3fbb7d01a9388076c033c036c4
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-02 01:52:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ def play(songs)
   if songs.include? resp
     puts "Playing #{resp}"
     
-  elsif songs[resp.to_i - 1].is_a? String
+  elsif songs[resp.to_i - 1] >= 0
     # puts "Resp: #{resp.to_i-1}"
     puts "Playing #{songs[resp.to_i - 1]}"
     
