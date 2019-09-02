b2e58ed5ca86d1d69943ea4db8131efc05b842d4
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-02 01:00:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -2,7 +2,11 @@
 
 def help
   
-puts ""
+puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 
   
 end
